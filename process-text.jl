a407f0d7993bf0640f78affc2a69998f81599fbf
smallcats <gschnei24@gmail.com>
2017-05-01 14:15:06 -0400

    Added text processing functions Changes to be committed: 	modified:   process-text.jl

--- process-text.jl
+++ process-text.jl
@@ -1,3 +1,38 @@
-function processtext(text)
-  matchall(r"(\w+)",lowercase(replace(text, r"([\r\n,\.\(\)!;:\?/])", s"")))
+function texttowords(text)
+  matchall(r"(\w+)",lowercase(replace(text, r"([\r\n,\.\(\)!;:\?/]|\ufeff)", s" ")))
+end
+
+function wordcount(wordvec)
+  worddict = Dict{String,Int64}();
+  for w in wordvec
+    worddict[w]=get(wordvec,w,0) + 1;
+  end
+  worddict
+end
+
+function adddicts(dicts)
+  sumdict = Dict{String,Int64}();
+  for d in dicts
+    for k in keys(dicts)
+      sumdict[k] = get(sumdict,k,0)+1;
+    end
+  end
+  sumdict
+end
+
+function wordfreq(worddict)
+  freqdict = Dict{String,Float64}();
+  totwords = sum(values(worddict));
+  for k in keys(worddict)
+    freqdict[k] = worddict[k]/totwords;
+  end
+  freqdict
+end
+
+function subdicts(d1, d2)
+  diffdict = d1;
+  for k in keys(d2)
+    diffdict[k] = get(diffdict, k,0)-d2[k];
+  end
+  diffdict
 end
